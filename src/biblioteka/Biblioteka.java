7d631508110b01a3dac1aad798e3a079d49e1a1f
kPlavsic <kplavsic4@gmail.com>
2026-03-23 12:59:28 +0100

    Uvedena logicka kontrola u klasu Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -8,19 +8,28 @@ import biblioteka.interfejs.BibliotekaInterface;
 public class Biblioteka implements BibliotekaInterface {
 
 	private List<Knjiga> knjige = new ArrayList<Knjiga>();
-	
+
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
 
+		if (knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga vec postoji");
 		knjige.add(knjiga);
-		
+
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if (knjiga == null)
+			throw new NullPointerException("Knjiga ne sme biti null");
+
+		if (!knjige.contains(knjiga))
+			throw new IllegalArgumentException("Knjiga vec postoji");
 
 		knjige.remove(knjiga);
-		
+
 	}
 
 	@Override
@@ -30,9 +39,12 @@ public class Biblioteka implements BibliotekaInterface {
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		if (autor == null && isbn <= 0 && naslov == null && izdavac == null)
+			throw new IllegalArgumentException("Morate uneti bar neki kriterijum pretrage.");
+
 		List<Knjiga> rezultati = new ArrayList<Knjiga>();
 		for (Knjiga k : knjige) {
-			if(k.getNaslov().toUpperCase().contains(naslov.toUpperCase())) {
+			if (k.getNaslov().toUpperCase().contains(naslov.toUpperCase())) {
 				rezultati.add(k);
 			}
 		}
